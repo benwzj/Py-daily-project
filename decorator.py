c28917ca73d86ca0572754da0fcea5e977f2f26a
benwzj <57896457+benwzj@users.noreply.github.com>
2023-02-15 15:16:53 +1100

    explain what is decorator

---
+++ decorator.py
@@ -0,0 +1,22 @@
+# what is decorator in python
+# we have a function:
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+
+    return wrapper
+
+# we can wrag function like this:
+def say_hi():
+    return "Hello There"
+
+print(uppercase_decorator(say_hi)())
+
+# use decorator, same result as above:
+@uppercase_decorator
+def de_say_hi():
+    return "Hello There using decorator"
+
+print(de_say_hi())
