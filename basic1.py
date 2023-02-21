eb5c6b77df44c1348f1f112dac49db54bf20b1bb
benwzj <57896457+benwzj@users.noreply.github.com>
2023-02-21 12:04:24 +1100

    class exercise

--- basic1.py
+++ basic1.py
@@ -1,4 +1,4 @@
-
+"""
 ### question1: reverse a string.
 
 # operate it as an array
@@ -14,8 +14,31 @@ print(str2)
 # using slice syntax:
 print(str1[::-1])
 
-### Remove Duplicates From a Python List
-# use dict.fromkeys(), don't use set
+### Question2: Remove Duplicates From a Python List
+# use dict.fromkeys(), don't use set, because set is unordered
 keywords = ['foo', 'bar', 'bar', 'foo', 'baz', 'foo']
 print (list(dict.fromkeys(keywords)))
+print (list(set(keywords)))
+"""
+### Question3: nonlocal concept
+def test_scopes():
+  x = 'spam'
+  def change_local():
+    x = 'local_spam'
+  def change_nonlocal():
+    nonlocal x
+    x = 'nonlocal_spam'
+  def change_global():
+    global x
+    x = "global_spam"
+  
+  x = "test_spam"
+  change_local()
+  print (x)
+  change_nonlocal()
+  print(x)
+  change_global()
+  print(x)
 
+test_scopes()
+print(x)
---
+++ class-exercise.py
@@ -0,0 +1,14 @@
+
+class Language:
+  def __init__(self, lang) -> None:
+    self.lang = lang
+  me = 'Language Class'
+  def message(self):
+    print("this is "+self.lang+", nice to meet you " + self.me)
+  def action(self):
+    print("let's action! " + self.lang)
+
+languages = [Language("python"), Language("javascript")]
+
+for language in languages:
+  language.message()
