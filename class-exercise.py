9511cf9502a2f0d5e880bd3166250199f413bea5
benwzj <57896457+benwzj@users.noreply.github.com>
2023-03-02 12:10:11 +1100

    update

--- class-exercise.py
+++ class-exercise.py
@@ -1,4 +1,5 @@
 
+'''
 class Language:
   def __init__(self, lang) -> None:
     self.lang = lang
@@ -13,4 +14,34 @@ languages = [Language("python"), Language("javascript")]
 for language in languages:
   language.message()
 
+'''
+# Abstract Base Classes
+from abc import ABC, abstractmethod
 
+class Vehicle(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def go_forward(self):
+        print ( "this is from ABC" )
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+    def go_forward(self):
+        return super().go_forward()
+
+class Truck(Vehicle):
+    def __init__(self):
+        super().__init__()
+    def go_forward(self):
+        print("this is a truck moving forward")
+
+# vehicle1 = Vehicle()
+
+car1 = Car()
+car1.go_forward()
+
+truck1 = Truck()
+truck1.go_forward()
